8cb590e8ad90a866edbd620051e0923f5b233b8d
fy <3320681251@qq.com>
2018-09-04 15:18:22 +0800

    Initial commit

---
+++ NettyDemo/src/main/java/com/holos/httpserver/TestServer.java
@@ -0,0 +1,34 @@
+package com.holos.httpserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+    public static void main(String[] args) throws InterruptedException {
+        //接收连接不做处理，转给worker
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        //接收boss的连接，做出相应处理。
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+
+    }
+}
